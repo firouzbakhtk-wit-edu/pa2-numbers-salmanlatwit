7032f8974fb467cd5a02483eb9c7cf8d9ecb79e1
Lily Salman <salmanl@wit.edu>
2024-10-10 08:03:19 -0400

    Assignment PA2 for CompSci

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -1,11 +1,130 @@
 package edu.wit.cs.comp1000;
 
-// TODO: document this class
+// Lily Salman
+
+import java.util.Scanner;
 public class PA2a {
 
 	// TODO: document this function
 	public static void main(String[] args) {
-		// TODO: write your code here
+		
+		// variables for code
+		Scanner input = new Scanner(System.in);
+		int a, b, c, d, e;
+		int numPos = 0;
+		int numNeg = 0;
+		int numFull = 5;
+		int sumPos = 0;
+		int sumNeg = 0;
+		int sumFull = 0;
+		double avrPos;
+		double avrNeg;
+		double avrFull;
+		
+		// allows user to input 5 numbers
+		System.out.printf("Enter five whole numbers: ");
+		a = input.nextInt();
+		b = input.nextInt();
+		c = input.nextInt();
+		d = input.nextInt();
+		e = input.nextInt();
+		
+		// determines if inputs are positive or negative
+		if (a > 0) {
+			numPos++;
+			sumPos = sumPos + a;
+		}
+		else {
+			numNeg++;
+			sumNeg = sumNeg + a;
+		}
+		if (b > 0) {
+			numPos++;
+			sumPos = sumPos + b;
+		}
+		else {
+			numNeg++;
+			sumNeg = sumNeg + b;
+		}
+		if (c > 0) {
+			numPos++;
+			sumPos = sumPos + c;
+		}
+		else {
+			numNeg++;
+			sumNeg = sumNeg + c;
+		}
+		if (d > 0) {
+			numPos++;
+			sumPos = sumPos + d;
+		}
+		else {
+			numNeg++;
+			sumNeg = sumNeg + d;
+		}
+		if (e > 0) {
+			numPos++;
+			sumPos = sumPos + e;
+		}
+		else {
+			numNeg++;
+			sumNeg = sumNeg + e;
+		}
+		
+		// equation for sum of all numbers
+		sumFull = sumFull + a + b + c + d + e;
+		
+		// calculations for averages positives/negatives
+		if (numPos == 0) {
+			avrPos = 0.0;
+		}
+		else {
+			avrPos = 1.0*sumPos / numPos;
+		}
+		
+		if (numNeg == 0) {
+			avrNeg = 0.0;
+		}
+		else {
+			avrNeg = 1.0*sumNeg/numNeg;
+		}
+		
+		// equation for full average
+		avrFull = 1.0*sumFull / numFull;
+		
+		// outputs answers based on number of numbers
+		if (numPos == 1) {
+			System.out.printf("The sum of the %d positive number: %d%n", numPos, sumPos);
+		}
+		else {
+			System.out.printf("The sum of the %d positive numbers: %d%n", numPos, sumPos);
+		}
+		
+		if (numNeg == 1) {
+			System.out.printf("The sum of the %d non-positive number: %d%n", numNeg, sumNeg);
+		}
+		else {
+			System.out.printf("The sum of the %d non-positive numbers: %d%n", numNeg, sumNeg);
+		}
+		
+		System.out.printf("The sum of the 5 numbers: %d%n", sumFull);
+		
+		if (numPos == 1) {
+			System.out.printf("The average of the %d positive number: %.2f%n", numPos, avrPos);
+		}
+		else {
+			System.out.printf("The average of the %d positive numbers: %.2f%n", numPos, avrPos);
+		}
+		
+		
+		if (numNeg == 1) {
+			System.out.printf("The average of the %d non-positive number: %.2f%n", numNeg, avrNeg);
+		}
+		else {
+			System.out.printf("The average of the %d non-positive numbers: %.2f%n", numNeg, avrNeg);
+		}
+		
+		System.out.printf("The average of the 5 numbers: %.2f%n", avrFull);
 	}
 
 }
--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -1,11 +1,59 @@
 package edu.wit.cs.comp1000;
 
-// TODO: document this class
+// Lily Salman
+
+import java.util.Scanner;
 public class PA2b {
 
 	// TODO: document this function
 	public static void main(String[] args) {
-		// TODO: write your code here
-	}
+		
+		Scanner input = new Scanner(System.in);
+		
+		// variables for code
+		double a, b, c;
+		double d = 0;
+		double r1 = 0;
+		double r2 = 0;
+		System.out.printf("Enter a b c: ");
+		a = input.nextDouble();
+		b = input.nextDouble();
+		c = input.nextDouble();
+		
+		//  discriminant equation
+		d = (b*b) - (4*a*c);
+		
+		// if imaginary root
+		if (d < 0) {
+			System.out.printf("Roots: imaginary%n");
+		}
+		// if one root
+		else if (d == 0) {
+			r1 = -b/(2*a);
+			System.out.printf("Root: %.2f%n", r1);
+		}
+		// if two roots
+		else {
+			r1 = (-b + Math.sqrt(d)) / (2*a);
+			r2 = (-b - Math.sqrt(d)) / (2*a);
+			
+			// if first root < second root
+			if (r1 < r2) {
+				System.out.printf("Roots: %.2f, %.2f%n", r1, r2);
+				}
+				else {
+				System.out.printf("Roots: %.2f, %.2f%n", r2, r1);
+				}
+		}
 
+
+	}
 }
+
+
+	
+		
+
+
+
+
